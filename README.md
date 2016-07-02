c0dcf2b1257a3fd41523dabac79aad29fda39ece
tashian <carl@tashian.com>
2016-07-01 21:41:05 -0700

    Fix the export code; improve docs

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ Simple JS-based wrapper for Medium JSON user feeds
 
 ## Usage
 
-The only function available is `getUser`. It accepts a medium.com username and returns a `Promise` for a user's most recent posts on Medium. You cannot paginate; only the latest posts are available.
+The only function available is `getUser`. It accepts a medium.com username and returns a `Promise` for a user's most recent posts on Medium. No pagination support yet; only the latest posts are available.
 
-    import medium from 'medium-node';
-    
-    let userFeed = medium.getUser('tashian');
-    userFeed.then((user) =>
-      console.log(user.posts[0]);
-    )
+    var medium = require('medium-node');
+
+    var userFeed = medium.getUser('tashian');
+    userFeed.then(function(latest) {
+      console.log(latest.references.Post);
+    });
+
+The response format is undocumented, but you can see an example [here](https://medium.com/@tashian/latest?format=json). This module only returns the "payload" section. Take a look at references.Post for the latest posts.
+
--- src/medium.js
+++ src/medium.js
@@ -1,24 +1,26 @@
 import request from 'request';
 
-export default function getUser(username) {
-  return new Promise(function(resolve, reject) {
-    if (username.charAt(0) == '@') {
-      username = username.substr(1);
-    }
+function getUser(username) {
+    return new Promise(function(resolve, reject) {
+      if (username.charAt(0) == '@') {
+        username = username.substr(1);
+      }
 
-    if (username.match(/[\/\\@&?]/)) {
-      return reject(new Error("Invalid characters in username " + username));
-    }
+      if (username.match(/[\/\\@&?]/)) {
+        return reject(new Error("Invalid characters in username " + username));
+      }
 
-    request('https://medium.com/@' + username + '/latest?format=json',
-      function(err, response) {
-        if (!err && response.statusCode == 200) {
-          resolve(
-            JSON.parse(response.body.replace('])}while(1);</x>', '')).payload
-          );
-        } else {
-          reject('Error: ' + response.statusCode)
-        }
-      });
-  });
-}
+      request('https://medium.com/@' + username + '/latest?format=json',
+        function(err, response) {
+          if (!err && response.statusCode == 200) {
+            resolve(
+              JSON.parse(response.body.replace('])}while(1);</x>', '')).payload
+            );
+          } else {
+            reject('Error: ' + response.statusCode)
+          }
+        });
+    });
+  }
+
+export { getUser }
